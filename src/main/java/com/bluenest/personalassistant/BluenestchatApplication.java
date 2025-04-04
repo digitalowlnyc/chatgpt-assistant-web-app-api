3a9a491db0d8550824bf506a40f5f2e1ebc18293
Bryan <git@bnest.co>
2025-04-04 10:41:49 -0400

    Spring boot initial commit

---
+++ src/main/java/com/bluenest/personalassistant/BluenestchatApplication.java
@@ -0,0 +1,13 @@
+package com.bluenest.personalassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluenestchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluenestchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluenest/personalassistant/BluenestchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluenest.personalassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluenestchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
